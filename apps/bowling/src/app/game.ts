4eac096bcfcca6be26dd2c5a98bc0e0f3e45d10b
alan890104 <alan890104@gmail.com>
2022-05-04 20:15:25 +0800

    test: forth green

--- apps/bowling/src/app/game.ts
+++ apps/bowling/src/app/game.ts
@@ -1,4 +1,3 @@
-// game.ts
 export class Game {
     private rolls: number[] = [];
     private currentRoll = 0;
@@ -10,6 +9,13 @@ export class Game {
       let score = 0;
       let frameIndex = 0;
       for (let frame = 0; frame < 10; frame++) {
+        // add this block
+        if (this.rolls[frameIndex] === 10) {
+          score += 10 + this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2];
+          frameIndex++;
+          continue;
+        }
+        // block-end
         if (this.isSpare(frameIndex)) {
           score += 10 + this.rolls[frameIndex + 2];
         } else {
@@ -24,3 +30,4 @@ export class Game {
       return this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10;
     }
   }
+  
