ede6afdd8065db39dcaee7188c474f1affef41ff
alan890104 <alan890104@gmail.com>
2022-05-04 20:15:53 +0800

    test: third reconstruct

--- apps/bowling/src/app/game.ts
+++ apps/bowling/src/app/game.ts
@@ -9,15 +9,13 @@ export class Game {
       let score = 0;
       let frameIndex = 0;
       for (let frame = 0; frame < 10; frame++) {
-        // add this block
-        if (this.rolls[frameIndex] === 10) {
-          score += 10 + this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2];
+        if (this.isStrike(frameIndex)) {
+          score += 10 + this.strikeBouns(frameIndex);
           frameIndex++;
           continue;
         }
-        // block-end
         if (this.isSpare(frameIndex)) {
-          score += 10 + this.rolls[frameIndex + 2];
+          score += 10 + this.spareBouns(frameIndex);
         } else {
           score += this.rolls[frameIndex] + this.rolls[frameIndex + 1];
         }
@@ -26,8 +24,20 @@ export class Game {
       return score;
     }
   
+    private isStrike(frameIndex: number) {
+      return this.rolls[frameIndex] === 10;
+    }
+  
     private isSpare(frameIndex: number) {
       return this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10;
+    }
+  
+    private strikeBouns(frameIndex: number) {
+      return this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2];
+    }
+  
+    private spareBouns(frameIndex: number) {
+      return this.rolls[frameIndex + 2];
     }
   }
   
