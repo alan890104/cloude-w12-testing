7cffaf70d03e1c12ee86cf138110d2613e5a1ac1
alan890104 <alan890104@gmail.com>
2022-05-04 20:11:40 +0800

    test: second red

--- apps/bowling/src/app/game.spec.ts
+++ apps/bowling/src/app/game.spec.ts
@@ -14,3 +14,12 @@ test('gutter game', () => {
   }
   expect(game.score).toBe(0);
 });
+
+// game.spec.ts
+test('all one', () => {
+  const game = new Game();
+  for (let i = 0; i < 20; i++) {
+    game.roll(1);
+  }
+  expect(game.score).toBe(20);
+});
