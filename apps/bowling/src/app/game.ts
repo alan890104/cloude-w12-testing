1b74fbd7aece84b33488c19c39fe4e6be46e49e8
alan890104 <alan890104@gmail.com>
2022-05-04 20:14:23 +0800

    test: second reconstruct

--- apps/bowling/src/app/game.ts
+++ apps/bowling/src/app/game.ts
@@ -2,23 +2,25 @@
 export class Game {
     private rolls: number[] = [];
     private currentRoll = 0;
-  
     roll(pins: number) {
       this.rolls[this.currentRoll++] = pins;
     }
   
     get score() {
       let score = 0;
-      let i = 0;
+      let frameIndex = 0;
       for (let frame = 0; frame < 10; frame++) {
-        if (this.rolls[i] + this.rolls[i + 1] === 10) {
-          score += 10 + this.rolls[i + 2];
+        if (this.isSpare(frameIndex)) {
+          score += 10 + this.rolls[frameIndex + 2];
         } else {
-          score += this.rolls[i] + this.rolls[i + 1];
+          score += this.rolls[frameIndex] + this.rolls[frameIndex + 1];
         }
-        i += 2;
+        frameIndex += 2;
       }
       return score;
     }
-  }
   
+    private isSpare(frameIndex: number) {
+      return this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10;
+    }
+  }
