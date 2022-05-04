a69b0444147b646786d7f580230b9d648da9c1f4
alan890104 <alan890104@gmail.com>
2022-05-04 20:13:59 +0800

    feat: third green

--- apps/bowling/src/app/game.ts
+++ apps/bowling/src/app/game.ts
@@ -1,11 +1,24 @@
 // game.ts
 export class Game {
-  private _score = 0;
-  roll(pins: number) {
-    this._score += pins;
+    private rolls: number[] = [];
+    private currentRoll = 0;
+  
+    roll(pins: number) {
+      this.rolls[this.currentRoll++] = pins;
+    }
+  
+    get score() {
+      let score = 0;
+      let i = 0;
+      for (let frame = 0; frame < 10; frame++) {
+        if (this.rolls[i] + this.rolls[i + 1] === 10) {
+          score += 10 + this.rolls[i + 2];
+        } else {
+          score += this.rolls[i] + this.rolls[i + 1];
+        }
+        i += 2;
+      }
+      return score;
+    }
   }
-
-  get score() {
-    return this._score;
-  }
-}
+  
