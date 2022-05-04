40bd624dfb0990065546d7a54179e2ab58004a25
alan890104 <alan890104@gmail.com>
2022-05-04 20:13:10 +0800

    test: first reconstruct

--- apps/bowling/src/app/game.spec.ts
+++ apps/bowling/src/app/game.spec.ts
@@ -1,25 +1,30 @@
+// game.spec.ts
 import { Game } from './game';
 
 describe('Game', () => {
-  it('should create an instance', () => {
-    expect(new Game()).toBeTruthy();
+  let game: Game;
+
+  beforeEach(() => {
+    game = new Game();
   });
-});
 
-// game.spec.ts
-test('gutter game', () => {
-  const game = new Game();
-  for (let i = 0; i < 20; i++) {
-    game.roll(0);
-  }
-  expect(game.score).toBe(0);
-});
+  it('should create an instance', () => {
+    expect(game).toBeTruthy();
+  });
 
-// game.spec.ts
-test('all one', () => {
-  const game = new Game();
-  for (let i = 0; i < 20; i++) {
-    game.roll(1);
+  test('all zero', () => {
+    rollMany(20, 0);
+    expect(game.score).toBe(0);
+  });
+
+  test('all one', () => {
+    rollMany(20, 1);
+    expect(game.score).toBe(20);
+  });
+
+  function rollMany(n: number, pins: number) {
+    for (let i = 0; i < n; i++) {
+      game.roll(pins);
+    }
   }
-  expect(game.score).toBe(20);
 });
